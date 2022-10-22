df5cd47a2369755de7ee6987060e4539d514ddcc
Muaz Harris <21FTT1242@student.pb.edu.bn>
2022-10-22 08:31:37 +0800

    main menu, button works but screen doesnt change

--- src/panel_test.java
+++ src/panel_test.java
@@ -1,55 +1,88 @@
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.*;
 
-public class panel_test {
+public class panel_test extends JFrame implements ActionListener {
+	
+	//make buttons
+	public JButton b1, b2, b3;
+	public JLabel lb;
+	
+	
+	//define buttons
+	public void createButton() {
+		
+		b1 = new JButton("Start Game");
+		b2 = new JButton("Game Rules");
+		b3 = new JButton("Exit Game");
+		lb = new JLabel("DIXIT");
+		
+		b1.setPreferredSize(new Dimension(200, 100));
+		b2.setPreferredSize(new Dimension(200, 100));
+		b3.setPreferredSize(new Dimension(200, 100));
+		
+		lb.setFont(new Font("Arial", Font.PLAIN, 32));
+		b1.setFont(new Font("Arial", Font.PLAIN, 24));
+		b2.setFont(new Font("Arial", Font.PLAIN, 24));
+		b3.setFont(new Font("Arial", Font.PLAIN, 24));
+		
+		b1.addActionListener(this);
+		b2.addActionListener(this);
+		b3.addActionListener(this);
 
-	public static void main(String[] args) {
-		JFrame frame = new JFrame("JFrame Example");
-		// set the JFrame to fullscreen
-		frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+	
+	//panels stuff
+	public panel_test() {
+		
+		createButton();
+		setLayout(new GridBagLayout());
+		
+		//set things to grids using this
+		GridBagConstraints gb = new GridBagConstraints();
+		
+		gb.gridx = 1;
+		gb.gridy = 0;
+	    gb.insets = new Insets(20, 0, 0, 0);
+	    add(lb, gb);
+		
+		gb.gridx = 1;
+		gb.gridy = 1;
+		add(b1, gb);
+		
+		gb.gridx = 1;
+		gb.gridy = 2;
+		add(b2, gb);
+		
+		gb.gridx = 1;
+		gb.gridy = 3;
+		add(b3, gb);
+		
+		setTitle("Dixit | Main Menu");
+		setSize(600,600);
+		setVisible(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		}
+	
+	//main method
+	public static void main(String[] args)  {
+		panel_test bruh = new panel_test();
+	}
 
-		// JFrame bounds when rescaled
-		frame.setBounds(100,100,1280,720);
-
-		// create a new panel
-		JPanel container = new JPanel();
-
-		container.setBackground(Color.black);
-		
-		JButton btn1 = new JButton("Start Game");    
-		JButton btn2 = new JButton("Game Rules");    
-		JButton btn3 = new JButton("Exit Game");
-		
-		btn1.setFont(new Font("Arial", Font.PLAIN, 32));
-		btn2.setFont(new Font("Arial", Font.PLAIN, 32));
-		btn3.setFont(new Font("Arial", Font.PLAIN, 32));
-		
-	    btn1.setPreferredSize(new Dimension(300, 100));
-	    btn2.setPreferredSize(new Dimension(300, 100));
-	    btn3.setPreferredSize(new Dimension(300, 100));
-	    
-		JPanel panel1 = new JPanel();
-		panel1.setBackground(Color.gray);
-
-		panel1.setLayout(new GridBagLayout());
-		frame.setLayout(new GridBagLayout());
-		GridBagConstraints gbc = new GridBagConstraints();
-		gbc.gridwidth = GridBagConstraints.REMAINDER;
-		gbc.fill = GridBagConstraints.HORIZONTAL;
-		gbc.insets = new Insets(10, 10, 10, 10);
-		
-		panel1.add(btn1, gbc);
-		panel1.add(btn2, gbc);
-		panel1.add(btn3, gbc);
-
-		container.add(panel1);
-		
-		
-		frame.add(container);
-		
-		frame.setVisible(true);
-		System.out.println("Test by Mus");
+	@Override
+	public void actionPerformed(ActionEvent btnclick) {
+		if(btnclick.getSource() == b1) {
+			System.out.println("Starting game!");
+		}
+		else if(btnclick.getSource() == b2) {
+			System.out.println("Game Rules!!");
+		}
+		else if(btnclick.getSource() == b3) {
+		System.exit(0); 
+		}
 	}
 
 }
